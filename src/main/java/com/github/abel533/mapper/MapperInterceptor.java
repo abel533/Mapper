deef4c4f67a3e5845a3b1cf32bc49313f312133a
abel533 <abel533@gmail.com>
2014-11-22 08:54:50 +0800

    增加MIT开源协议.

--- src/main/java/com/github/abel533/mapper/MapperInterceptor.java
+++ src/main/java/com/github/abel533/mapper/MapperInterceptor.java
@@ -1,3 +1,27 @@
+/*
+	The MIT License (MIT)
+
+	Copyright (c) 2014 abel533@gmail.com
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+*/
+
 package com.github.abel533.mapper;
 
 import org.apache.ibatis.builder.annotation.ProviderSqlSource;
@@ -11,6 +35,7 @@ import java.util.Properties;
 
 /**
  * 通用Mapper拦截器
+ * <p>项目地址 : <a href="https://github.com/abel533/Mapper" target="_blank">https://github.com/abel533/Mapper</a></p>
  *
  * @author liuzh
  */
@@ -20,7 +45,8 @@ import java.util.Properties;
 })
 public class MapperInterceptor implements Interceptor {
 
-    private final MapperHelper mapperHelper = new MapperHelper();;
+    private final MapperHelper mapperHelper = new MapperHelper();
+    ;
 
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
@@ -51,7 +77,7 @@ public class MapperInterceptor implements Interceptor {
             }
         }
         //只有select和delete通过PK操作时需要处理入参
-        mapperHelper.processParameterObject(ms,objects);
+        mapperHelper.processParameterObject(ms, objects);
         return invocation.proceed();
     }
 
