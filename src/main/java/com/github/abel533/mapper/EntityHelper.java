c2e297fb9f7fc5fcfba526fb986810f00bbae343
isea533 <abel533@gmail.com>
2014-12-07 16:51:42 +0800

    驼峰和下划线转换代码改为一个效率更高的方法。

--- src/main/java/com/github/abel533/mapper/EntityHelper.java
+++ src/main/java/com/github/abel533/mapper/EntityHelper.java
@@ -28,8 +28,6 @@ import javax.persistence.*;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * 实体类工具类
@@ -308,33 +306,67 @@ public class EntityHelper {
      * 将驼峰风格替换为下划线风格
      */
     public static String camelhumpToUnderline(String str) {
-        Matcher matcher = Pattern.compile("[A-Z]").matcher(str);
-        StringBuilder builder = new StringBuilder(str);
-        for (int i = 0; matcher.find(); i++) {
-            builder.replace(matcher.start() + i, matcher.end() + i, "_" + matcher.group().toLowerCase());
+        final int size;
+        final char[] chars;
+        final StringBuilder sb = new StringBuilder(
+                (size = (chars = str.toCharArray()).length) * 3 / 2 + 1);
+        char c;
+        for (int i = 0; i < size; i++) {
+            c = chars[i];
+            if (isLowercaseAlpha(c)) {
+                sb.append(toUpperAscii(c));
+            } else {
+                sb.append('_').append(c);
+            }
         }
-        if (builder.charAt(0) == '_') {
-            builder.deleteCharAt(0);
-        }
-        return builder.toString();
+        return sb.charAt(0) == '_'? sb.substring(1): sb.toString();
     }
 
     /**
      * 将下划线风格替换为驼峰风格
-     *
-     * @param str
-     * @return
      */
-    public static String underlineToCamelhump(String str) {
-        Matcher matcher = Pattern.compile("_[a-z]").matcher(str);
-        StringBuilder builder = new StringBuilder(str);
-        for (int i = 0; matcher.find(); i++) {
-            builder.replace(matcher.start() - i, matcher.end() - i, matcher.group().substring(1).toUpperCase());
+    public static String underlineToCamelhump(String name) {
+        char[] buffer = name.toCharArray();
+        int count = 0;
+        boolean lastUnderscore = false;
+        for (int i = 0; i < buffer.length; i++) {
+            char c = buffer[i];
+            if (c == '_') {
+                lastUnderscore = true;
+            } else {
+                c = (lastUnderscore && count != 0) ? toUpperAscii(c) : toLowerAscii(c);
+                buffer[count++] = c;
+                lastUnderscore = false;
+            }
         }
-        if (Character.isUpperCase(builder.charAt(0))) {
-            builder.replace(0, 1, String.valueOf(Character.toLowerCase(builder.charAt(0))));
+        if (count != buffer.length) {
+            buffer = subarray(buffer, 0, count);
         }
-        return builder.toString();
+        return new String(buffer);
+    }
+
+    public static char[] subarray(char[] src, int offset, int len) {
+        char[] dest = new char[len];
+        System.arraycopy(src, offset, dest, 0, len);
+        return dest;
+    }
+
+    public static boolean isLowercaseAlpha(char c) {
+        return (c >= 'a') && (c <= 'z');
+    }
+
+    public static char toUpperAscii(char c) {
+        if (isLowercaseAlpha(c)) {
+            c -= (char) 0x20;
+        }
+        return c;
+    }
+
+    public static char toLowerAscii(char c) {
+        if ((c >= 'A') && (c <= 'Z')) {
+            c += (char) 0x20;
+        }
+        return c;
     }
 
     /**
