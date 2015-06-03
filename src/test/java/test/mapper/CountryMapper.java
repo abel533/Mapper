53d0a8c2435c0ac19445bffa212f4edbee7924e3
abel533 <abel533@gmail.com>
2015-06-03 21:42:10 +0800

    删除MBG生成的不需要的代码。

--- src/test/java/test/mapper/CountryMapper.java
+++
@@ -1,8 +0,0 @@
-package test.mapper;
-
-import com.github.abel533.hsqldb.HsqldbMapper;
-import com.github.abel533.mapper.Mapper;
-import test.model.Country;
-
-public interface CountryMapper extends HsqldbMapper<Country>, Mapper<Country> {
-}
--- src/test/java/test/mapper/UserInfoMapper.java
+++
@@ -1,8 +0,0 @@
-package test.mapper;
-
-import com.github.abel533.hsqldb.HsqldbMapper;
-import com.github.abel533.mapper.Mapper;
-import test.model.UserInfo;
-
-public interface UserInfoMapper extends HsqldbMapper<UserInfo>, Mapper<UserInfo> {
-}
