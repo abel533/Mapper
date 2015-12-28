2eab92f828d92d1c68099ee09b7b425a78762522
isea533 <abel533@gmail.com>
2015-12-28 23:36:33 +0800

    解决SpecialProvider不支持insertable的bug#77

--- src/main/java/tk/mybatis/mapper/provider/SpecialProvider.java
+++ src/main/java/tk/mybatis/mapper/provider/SpecialProvider.java
@@ -32,6 +32,8 @@ import tk.mybatis.mapper.mapperhelper.MapperHelper;
 import tk.mybatis.mapper.mapperhelper.MapperTemplate;
 import tk.mybatis.mapper.mapperhelper.SqlHelper;
 
+import java.util.Set;
+
 /**
  * SpecialProvider实现类，特殊方法实现类
  *
@@ -50,26 +52,22 @@ public class SpecialProvider extends MapperTemplate {
      */
     public String insertList(MappedStatement ms) {
         final Class<?> entityClass = getEntityClass(ms);
-        EntityTable table = EntityHelper.getEntityTable(entityClass);
         //开始拼sql
         StringBuilder sql = new StringBuilder();
         sql.append(SqlHelper.insertIntoTable(entityClass, tableName(entityClass)));
         sql.append(SqlHelper.insertColumns(entityClass, true, false, false));
         sql.append(" VALUES ");
         sql.append("<foreach collection=\"list\" item=\"record\" separator=\",\" >");
-        sql.append("(");
-        boolean first = true;
-        for (EntityColumn column : table.getEntityClassColumns()) {
-            if (column.isId()) {
-                continue;
+        sql.append("<trim prefix=\"VALUES (\" suffix=\")\" suffixOverrides=\",\">");
+        //获取全部列
+        Set<EntityColumn> columnList = EntityHelper.getColumns(entityClass);
+        //当某个列有主键策略时，不需要考虑他的属性是否为空，因为如果为空，一定会根据主键策略给他生成一个值
+        for (EntityColumn column : columnList) {
+            if (!column.isId() && column.isInsertable()) {
+                sql.append(column.getColumnHolder("record") + ",");
             }
-            if (!first) {
-                sql.append(",");
-            }
-            sql.append(column.getColumnHolder("record"));
-            first = false;
         }
-        sql.append(")");
+        sql.append("</trim>");
         sql.append("</foreach>");
         return sql.toString();
     }
