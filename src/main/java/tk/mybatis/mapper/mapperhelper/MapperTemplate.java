84617669d3a3d51b9a38af9e37f42c18bf88c67b
isea533 <abel533@gmail.com>
2017-09-03 21:18:57 +0800

    msId相关的3个方法独立出来，直接引入静态方法。

--- src/main/java/tk/mybatis/mapper/mapperhelper/MapperTemplate.java
+++ src/main/java/tk/mybatis/mapper/mapperhelper/MapperTemplate.java
@@ -43,6 +43,9 @@ import java.text.MessageFormat;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
+import static tk.mybatis.mapper.util.MsUtil.getMapperClass;
+import static tk.mybatis.mapper.util.MsUtil.getMethodName;
+
 /**
  * 通用Mapper模板类，扩展通用Mapper时需要继承该类
  *
@@ -58,44 +61,6 @@ public abstract class MapperTemplate {
     public MapperTemplate(Class<?> mapperClass, MapperHelper mapperHelper) {
         this.mapperClass = mapperClass;
         this.mapperHelper = mapperHelper;
-    }
-
-    /**
-     * 根据msId获取接口类
-     *
-     * @param msId
-     * @return
-     */
-    public static Class<?> getMapperClass(String msId) {
-        if (msId.indexOf(".") == -1) {
-            throw new MapperException("当前MappedStatement的id=" + msId + ",不符合MappedStatement的规则!");
-        }
-        String mapperClassStr = msId.substring(0, msId.lastIndexOf("."));
-        try {
-            return Class.forName(mapperClassStr);
-        } catch (ClassNotFoundException e) {
-            return null;
-        }
-    }
-
-    /**
-     * 获取执行的方法名
-     *
-     * @param ms
-     * @return
-     */
-    public static String getMethodName(MappedStatement ms) {
-        return getMethodName(ms.getId());
-    }
-
-    /**
-     * 获取执行的方法名
-     *
-     * @param msId
-     * @return
-     */
-    public static String getMethodName(String msId) {
-        return msId.substring(msId.lastIndexOf(".") + 1);
     }
 
     /**
---
+++ src/main/java/tk/mybatis/mapper/util/MsUtil.java
@@ -0,0 +1,50 @@
+package tk.mybatis.mapper.util;
+
+import org.apache.ibatis.mapping.MappedStatement;
+import tk.mybatis.mapper.MapperException;
+
+/**
+ * @author liuzh
+ * @since 2017/7/9.
+ */
+public class MsUtil {
+
+    /**
+     * 根据msId获取接口类
+     *
+     * @param msId
+     * @return
+     */
+    public static Class<?> getMapperClass(String msId) {
+        if (msId.indexOf(".") == -1) {
+            throw new MapperException("当前MappedStatement的id=" + msId + ",不符合MappedStatement的规则!");
+        }
+        String mapperClassStr = msId.substring(0, msId.lastIndexOf("."));
+        try {
+            return Class.forName(mapperClassStr);
+        } catch (ClassNotFoundException e) {
+            return null;
+        }
+    }
+
+    /**
+     * 获取执行的方法名
+     *
+     * @param ms
+     * @return
+     */
+    public static String getMethodName(MappedStatement ms) {
+        return getMethodName(ms.getId());
+    }
+
+    /**
+     * 获取执行的方法名
+     *
+     * @param msId
+     * @return
+     */
+    public static String getMethodName(String msId) {
+        return msId.substring(msId.lastIndexOf(".") + 1);
+    }
+
+}
