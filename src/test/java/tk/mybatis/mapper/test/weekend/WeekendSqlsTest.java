4f410bd4f902d69f013e1500d9dd4cb0670cbaa8
isea533 <abel533@gmail.com>
2018-01-22 21:51:59 +0800

    增加MIT注释，指定javadoc版本。

--- src/test/java/tk/mybatis/mapper/test/weekend/WeekendSqlsTest.java
+++ src/test/java/tk/mybatis/mapper/test/weekend/WeekendSqlsTest.java
@@ -1,17 +1,28 @@
-package tk.mybatis.mapper.test.weekend;
-
 /*
-import org.apache.ibatis.session.SqlSession;
-import org.junit.Assert;
-import tk.mybatis.mapper.entity.Example;
-import tk.mybatis.mapper.mapper.CountryMapper;
-import tk.mybatis.mapper.mapper.MybatisHelper;
-import tk.mybatis.mapper.model.Country;
-import tk.mybatis.mapper.util.Sqls;
-import tk.mybatis.mapper.weekend.WeekendSqls;
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2017 abel533@gmail.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
-import java.util.List;
-*/
+package tk.mybatis.mapper.test.weekend;
 
 /**
  * 测试WeekendSql构建者模式类 由于方法引用需要jdk8
@@ -31,8 +42,8 @@ import java.util.List;
  * @author XuYin
  */
 public class WeekendSqlsTest {
-    //@Test
     /*
+    @Test
     public void testWeekend() {
         SqlSession sqlSession = MybatisHelper.getSqlSession();
         try {
@@ -48,7 +59,7 @@ public class WeekendSqlsTest {
         }
     }
 
-    //@Test
+    @Test
     public void testWeekendComplex() {
         SqlSession sqlSession = MybatisHelper.getSqlSession();
         try {
