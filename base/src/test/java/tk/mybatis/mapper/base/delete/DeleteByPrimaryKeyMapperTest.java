95d47cf226360c7f57f96f85545b57bf5e4bd9bd
abel533 <abel533@gmail.com>
2018-02-21 20:32:58 +0800

    增加测试

---
+++ base/src/test/java/tk/mybatis/mapper/base/delete/DeleteByPrimaryKeyMapperTest.java
@@ -0,0 +1,28 @@
+package tk.mybatis.mapper.base.delete;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Assert;
+import org.junit.Test;
+import tk.mybatis.mapper.base.BaseTest;
+import tk.mybatis.mapper.base.CountryMapper;
+
+public class DeleteByPrimaryKeyMapperTest extends BaseTest {
+
+
+
+    @Test
+    public void testDeleteByPrimaryKey(){
+        final SqlSession sqlSession = getSqlSession();
+        final CountryMapper mapper = sqlSession.getMapper(CountryMapper.class);
+        Assert.assertEquals(183, mapper.selectAll().size());
+        Assert.assertEquals(1, mapper.deleteByPrimaryKey(1L));
+        Assert.assertEquals(182, mapper.selectAll().size());
+        Assert.assertEquals(1, mapper.deleteByPrimaryKey(2));
+        Assert.assertEquals(181, mapper.selectAll().size());
+        Assert.assertEquals(1, mapper.deleteByPrimaryKey("3"));
+        Assert.assertEquals(180, mapper.selectAll().size());
+        Assert.assertEquals(1, mapper.deleteByPrimaryKey(1));
+        Assert.assertEquals(180, mapper.selectAll().size());
+    }
+
+}
