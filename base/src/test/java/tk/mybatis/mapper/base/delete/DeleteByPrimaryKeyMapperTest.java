7493a12ce3babfa187a57ef83458579b3ad3b11f
abel533 <abel533@gmail.com>
2018-02-24 10:05:56 +0800

    完善测试，增加二级缓存的测试。 顺便发现个 MyBatis 的 Bug，目前配置二级缓存时，XML和接口混合使用时，只能在接口中引用XML中的缓存配置，参考CountryCacheRefMapper.java 和 CountryCacheRefMapper.xml。反过来就会出错。

--- base/src/test/java/tk/mybatis/mapper/base/delete/DeleteByPrimaryKeyMapperTest.java
+++ base/src/test/java/tk/mybatis/mapper/base/delete/DeleteByPrimaryKeyMapperTest.java
@@ -16,11 +16,14 @@ public class DeleteByPrimaryKeyMapperTest extends BaseTest {
             Assert.assertEquals(183, mapper.selectAll().size());
             Assert.assertEquals(1, mapper.deleteByPrimaryKey(1L));
             Assert.assertEquals(182, mapper.selectAll().size());
+
             Assert.assertEquals(1, mapper.deleteByPrimaryKey(2));
             Assert.assertEquals(181, mapper.selectAll().size());
+
             Assert.assertEquals(1, mapper.deleteByPrimaryKey("3"));
             Assert.assertEquals(180, mapper.selectAll().size());
-            Assert.assertEquals(1, mapper.deleteByPrimaryKey(1));
+
+            Assert.assertEquals(0, mapper.deleteByPrimaryKey(1));
             Assert.assertEquals(180, mapper.selectAll().size());
         } finally {
             sqlSession.close();
