6749d3afcf5d8a7edc0f051b1d6e5a78662d0e9f
isea533 <abel533@gmail.com>
2018-02-25 11:54:25 +0800

    增加完善的 DATE, TIME, DATETIME 类型测试

---
+++ base/src/test/java/tk/mybatis/mapper/issues/_216_datetime/TimeModel2Mapper.java
@@ -0,0 +1,10 @@
+package tk.mybatis.mapper.issues._216_datetime;
+
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author liuzh
+ */
+public interface TimeModel2Mapper extends Mapper<TimeModel2> {
+
+}
