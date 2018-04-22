e03d574fddb73491cb0cf7913fcd554f0d6311b5
isea533 <abel533@gmail.com>
2018-04-22 15:43:53 +0800

    增加 RegisterMapper 注解，此问题应该是之前导致常见问题的主要原因

--- extra/src/main/java/tk/mybatis/mapper/additional/insert/InsertListMapper.java
+++ extra/src/main/java/tk/mybatis/mapper/additional/insert/InsertListMapper.java
@@ -25,6 +25,7 @@
 package tk.mybatis.mapper.additional.insert;
 
 import org.apache.ibatis.annotations.InsertProvider;
+import tk.mybatis.mapper.annotation.RegisterMapper;
 
 import java.util.List;
 
@@ -35,12 +36,15 @@ import java.util.List;
  * @author liuzh
  * @since 3.5.0
  */
+@RegisterMapper
 public interface InsertListMapper<T> {
 
     /**
      * 批量插入，支持批量插入的数据库可以使用，例如MySQL,H2等
      * <p>
      * 不支持主键策略，插入前需要设置好主键的值
+     * <p>
+     * 特别注意：2018-04-22 后，该方法支持 @KeySql 注解的 genId 方式
      *
      * @param recordList
      * @return
