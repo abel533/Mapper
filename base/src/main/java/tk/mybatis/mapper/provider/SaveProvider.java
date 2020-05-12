70a54b7983b889781d790263d7cf28640eecf45b
yangb <523084685@qq.com>
2020-05-12 18:02:41 +0800

    feature: :tada: 通用Mapper接口,保存(save). <p>判断主键是否存在, 如果存在且不为空执行update语句,如果主键不存在或为空, 执行insert语句</p>

---
+++ base/src/main/java/tk/mybatis/mapper/provider/SaveProvider.java
@@ -0,0 +1,130 @@
+package tk.mybatis.mapper.provider;
+
+import org.apache.ibatis.mapping.MappedStatement;
+import tk.mybatis.mapper.MapperException;
+import tk.mybatis.mapper.entity.EntityColumn;
+import tk.mybatis.mapper.mapperhelper.*;
+
+import java.lang.reflect.Field;
+import java.util.Set;
+
+/**
+ * 保存实现类
+ * Created by YangBin on 2020/5/12
+ * Copyright (c) 2020 杨斌 All rights reserved.
+ */
+public class SaveProvider extends MapperTemplate {
+
+
+    public SaveProvider(Class<?> mapperClass, MapperHelper mapperHelper) {
+        super(mapperClass, mapperHelper);
+    }
+
+    /**
+     * 保存策略: 如果主键不为空则更新记录, 如果没有主键或者主键为空,则插入.
+     * @param ms
+     * @return
+     */
+    public String save(MappedStatement ms){
+
+        Class<?> entityClass = getEntityClass(ms);
+        Field[] fields = entityClass.getFields();
+        StringBuilder sql = new StringBuilder();
+
+        Set<EntityColumn> columnList = EntityHelper.getPKColumns(entityClass);
+        if (columnList.size() == 1) {
+            EntityColumn column = (EntityColumn)columnList.iterator().next();
+            String id = column.getColumn();
+            sql.append("<choose>");
+            sql.append("<when test='" + id + "!=null'>");
+            sql.append(updateByPrimaryKey(ms));
+            sql.append("</when>");
+            sql.append("<otherwise>");
+            sql.append(insert(ms));
+            sql.append("</otherwise>");
+            sql.append("</choose>");
+            return sql.toString();
+        }
+        return insert(ms);
+    }
+
+    /**
+     * 通过主键更新全部字段
+     *
+     * @param ms
+     */
+    public String updateByPrimaryKey(MappedStatement ms) {
+        Class<?> entityClass = getEntityClass(ms);
+        StringBuilder sql = new StringBuilder();
+        sql.append(SqlHelper.updateTable(entityClass, tableName(entityClass)));
+        sql.append(SqlHelper.updateSetColumns(entityClass, null, false, false));
+        sql.append(SqlHelper.wherePKColumns(entityClass, true));
+        return sql.toString();
+    }
+
+    public String insert(MappedStatement ms) {
+        Class<?> entityClass = getEntityClass(ms);
+        StringBuilder sql = new StringBuilder();
+        //获取全部列
+        Set<EntityColumn> columnList = EntityHelper.getColumns(entityClass);
+        processKey(sql, entityClass, ms, columnList);
+        sql.append(SqlHelper.insertIntoTable(entityClass, tableName(entityClass)));
+        sql.append(SqlHelper.insertColumns(entityClass, false, false, false));
+        sql.append("<trim prefix=\"VALUES(\" suffix=\")\" suffixOverrides=\",\">");
+        for (EntityColumn column : columnList) {
+            if (!column.isInsertable()) {
+                continue;
+            }
+            //优先使用传入的属性值,当原属性property!=null时，用原属性
+            //自增的情况下,如果默认有值,就会备份到property_cache中,所以这里需要先判断备份的值是否存在
+            if (column.isIdentity()) {
+                sql.append(SqlHelper.getIfCacheNotNull(column, column.getColumnHolder(null, "_cache", ",")));
+            } else {
+                //其他情况值仍然存在原property中
+                sql.append(SqlHelper.getIfNotNull(column, column.getColumnHolder(null, null, ","), isNotEmpty()));
+            }
+            //当属性为null时，如果存在主键策略，会自动获取值，如果不存在，则使用null
+            if (column.isIdentity()) {
+                sql.append(SqlHelper.getIfCacheIsNull(column, column.getColumnHolder() + ","));
+            } else {
+                //当null的时候，如果不指定jdbcType，oracle可能会报异常，指定VARCHAR不影响其他
+                sql.append(SqlHelper.getIfIsNull(column, column.getColumnHolder(null, null, ","), isNotEmpty()));
+            }
+        }
+        sql.append("</trim>");
+        return sql.toString();
+    }
+
+    private void processKey(StringBuilder sql, Class<?> entityClass, MappedStatement ms, Set<EntityColumn> columnList){
+        //Identity列只能有一个
+        Boolean hasIdentityKey = false;
+        //先处理cache或bind节点
+        for (EntityColumn column : columnList) {
+            if (column.isIdentity()) {
+                //这种情况下,如果原先的字段有值,需要先缓存起来,否则就一定会使用自动增长
+                //这是一个bind节点
+                sql.append(SqlHelper.getBindCache(column));
+                //如果是Identity列，就需要插入selectKey
+                //如果已经存在Identity列，抛出异常
+                if (hasIdentityKey) {
+                    //jdbc类型只需要添加一次
+                    if (column.getGenerator() != null && column.getGenerator().equals("JDBC")) {
+                        continue;
+                    }
+                    throw new MapperException(ms.getId() + "对应的实体类" + entityClass.getCanonicalName() + "中包含多个MySql的自动增长列,最多只能有一个!");
+                }
+                //插入selectKey
+                SelectKeyHelper.newSelectKeyMappedStatement(ms, column, entityClass, isBEFORE(), getIDENTITY(column));
+                hasIdentityKey = true;
+            } else if(column.getGenIdClass() != null){
+                sql.append("<bind name=\"").append(column.getColumn()).append("GenIdBind\" value=\"@tk.mybatis.mapper.genid.GenIdUtil@genId(");
+                sql.append("_parameter").append(", '").append(column.getProperty()).append("'");
+                sql.append(", @").append(column.getGenIdClass().getCanonicalName()).append("@class");
+                sql.append(", '").append(tableName(entityClass)).append("'");
+                sql.append(", '").append(column.getColumn()).append("')");
+                sql.append("\"/>");
+            }
+
+        }
+    }
+}
