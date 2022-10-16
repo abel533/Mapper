79d313a7ca6cba6c5d5323746fb83ed5744180a1
abel533 <abel533@gmail.com>
2022-10-16 13:39:03 +0800

    去掉 #666 添加的排序

--- core/src/main/java/tk/mybatis/mapper/mapperhelper/FieldHelper.java
+++ core/src/main/java/tk/mybatis/mapper/mapperhelper/FieldHelper.java
@@ -188,12 +188,6 @@ public class FieldHelper {
                 return fieldList;
             }
             Field[] fields = entityClass.getDeclaredFields();
-            Arrays.sort(fields, new Comparator<Object>() {
-                @Override
-                public int compare(Object a, Object b) {
-                    return a.toString().compareTo(b.toString());
-                }
-            });
             int index = 0;
             for (int i = 0; i < fields.length; i++) {
                 Field field = fields[i];
