1a99fe60630aabeedcf0c23a065973040da653c2
abel533 <abel533@gmail.com>
2022-10-16 19:43:11 +0800

    解决 pr#685 中的问题

--- weekend/src/main/java/tk/mybatis/mapper/weekend/WeekendSqls.java
+++ weekend/src/main/java/tk/mybatis/mapper/weekend/WeekendSqls.java
@@ -278,12 +278,12 @@ public class WeekendSqls<T> implements tk.mybatis.mapper.entity.SqlsCriteria {
         return this.orNotBetween(Reflections.fnToFieldName(fn), value1, value2);
     }
 
-    public WeekendSqls<T> orLike(String property, Object value) {
+    public WeekendSqls<T> orLike(String property, String value) {
         this.criteria.getCriterions().add(new Criterion(property, value, "like", "or"));
         return this;
     }
 
-    public WeekendSqls<T> orLike(Fn<T, Object> fn, Object value) {
+    public WeekendSqls<T> orLike(Fn<T, Object> fn, String value) {
         return this.orLike(Reflections.fnToFieldName(fn), value);
     }
 
