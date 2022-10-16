7c042b601b791477326c19be6211b92f18aaaee9
abel533 <abel533@gmail.com>
2022-10-16 15:27:12 +0800

    去掉 #386 的两个TODO，批量操作不考虑乐观锁。

--- base/src/main/java/tk/mybatis/mapper/provider/ExampleProvider.java
+++ base/src/main/java/tk/mybatis/mapper/provider/ExampleProvider.java
@@ -144,11 +144,6 @@ public class ExampleProvider extends MapperTemplate {
         sql.append(SqlHelper.updateTable(entityClass, tableName(entityClass), "example"));
         sql.append(SqlHelper.updateSetColumnsIgnoreVersion(entityClass, "record", true, isNotEmpty()));
         sql.append(SqlHelper.updateByExampleWhereClause());
-        //TODO 加入乐观锁查询条件
-        //乐观锁条件，加在example后面，有两个问题
-        // 1.会和example的条件混在一起，导致or和and逻辑混乱
-        // 2.如果example为空，会缺少WHERE
-        //sql.append(SqlHelper.whereVersion(entityClass, "record"));
         return sql.toString();
     }
 
@@ -171,11 +166,6 @@ public class ExampleProvider extends MapperTemplate {
         sql.append(SqlHelper.updateTable(entityClass, tableName(entityClass), "example"));
         sql.append(SqlHelper.updateSetColumnsIgnoreVersion(entityClass, "record", false, false));
         sql.append(SqlHelper.updateByExampleWhereClause());
-        //TODO 加入乐观锁查询条件
-        //乐观锁条件，加在example后面，有两个问题
-        // 1.会和example的条件混在一起，导致or和and逻辑混乱
-        // 2.如果example为空，会缺少WHERE
-        //sql.append(SqlHelper.whereVersion(entityClass, "record"));
         return sql.toString();
     }
 
