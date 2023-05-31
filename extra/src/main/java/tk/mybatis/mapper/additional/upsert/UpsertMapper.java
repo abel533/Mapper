3ea472c41ff3e3343094090d75d5379111f60c4e
abel533 <abel533@gmail.com>
2023-05-31 21:12:34 +0800

    UpsertMapper 添加@RegisterMapper注解，fixed #878

--- extra/src/main/java/tk/mybatis/mapper/additional/upsert/UpsertMapper.java
+++ extra/src/main/java/tk/mybatis/mapper/additional/upsert/UpsertMapper.java
@@ -1,7 +1,9 @@
 package tk.mybatis.mapper.additional.upsert;
 
 import org.apache.ibatis.annotations.UpdateProvider;
+import tk.mybatis.mapper.annotation.RegisterMapper;
 
+@RegisterMapper
 public interface UpsertMapper<T> {
 
     @UpdateProvider(
