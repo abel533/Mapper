30e8c35ed8166e4b4f176a96030339b391c27206
Hao Yuan <y1997h@126.com>
2024-02-28 09:08:23 +0800

    test

---
+++ base/src/test/java/tk/mybatis/mapper/helper/MultipleMapperProviderTest.java
@@ -0,0 +1,37 @@
+package tk.mybatis.mapper.helper;
+
+import org.apache.ibatis.annotations.InsertProvider;
+import org.apache.ibatis.annotations.SelectProvider;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Assert;
+import org.junit.Test;
+import tk.mybatis.mapper.annotation.RegisterMapper;
+import tk.mybatis.mapper.mapper.CountryMultipleMapper;
+import tk.mybatis.mapper.mapper.MybatisHelper;
+import tk.mybatis.mapper.model.Country;
+import tk.mybatis.mapper.provider.base.BaseInsertProvider;
+import tk.mybatis.mapper.provider.base.BaseSelectProvider;
+
+import java.util.List;
+
+/**
+ * @author yuanhao
+ */
+public class MultipleMapperProviderTest {
+    @Test
+    public void test() {
+        SqlSession sqlSession = MybatisHelper.getSqlSession();
+        try {
+            CountryMultipleMapper mapper = sqlSession.getMapper(CountryMultipleMapper.class);
+
+            Country country = new Country();
+            country.setId(200);
+            country.setCountrycode("AB");
+            mapper.insert(country);
+            List<Country> countryList = mapper.select(country);
+            Assert.assertEquals("AB", countryList.get(0).getCountrycode());
+        } finally {
+            sqlSession.close();
+        }
+    }
+}
