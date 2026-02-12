72693a7430a86c3a2e8948ddbb62ea3da7337943
Joe798 <592144581@qq.com>
2026-02-12 11:50:25 +0800

    feat: add UserHump entity and mapper for aggregation with camel case support. pr#743

---
+++ extra/src/test/java/tk/mybatis/mapper/additional/aggregation/UserHumpMapper.java
@@ -0,0 +1,6 @@
+package tk.mybatis.mapper.additional.aggregation;
+
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserHumpMapper extends Mapper<UserHump>, AggregationMapper<UserHump> {
+}
