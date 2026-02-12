44d1eb4d2c53b49916bd72d94f06ecb21fcf2511
Joe798 <592144581@qq.com>
2026-02-12 11:50:25 +0800

    feat: add UserHump entity and mapper for aggregation with camel case support. pr#743

--- extra/src/test/java/tk/mybatis/mapper/additional/aggregation/CreateDB.sql
+++ extra/src/test/java/tk/mybatis/mapper/additional/aggregation/CreateDB.sql
@@ -19,3 +19,19 @@ INSERT INTO user (id, name, role)
 VALUES (5, 'Andorra', 'USER');
 INSERT INTO user (id, name, role)
 VALUES (6, 'Anguilla', 'USER');
+
+drop table user_hump if exists;
+
+create table user_hump (
+  id integer NOT NULL PRIMARY KEY,
+  user_name varchar(32),
+  user_role VARCHAR(32)
+);
+
+INSERT INTO user_hump (id, user_name, user_role) VALUES (1, 'Angola', 'Admin');
+INSERT INTO user_hump (id, user_name, user_role) VALUES (2, 'Afghanistan', 'Admin');
+INSERT INTO user_hump (id, user_name, user_role) VALUES (3, 'Albania', 'Admin');
+INSERT INTO user_hump (id, user_name, user_role) VALUES (4, 'Algeria', 'USER');
+INSERT INTO user_hump (id, user_name, user_role) VALUES (5, 'Andorra', 'USER');
+INSERT INTO user_hump (id, user_name, user_role) VALUES (6, 'Anguilla', 'USER');
+
